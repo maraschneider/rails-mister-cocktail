85fefe23e99dfb429e3d2b8e6c97b482f4a1cc36
Maraike Schneider <maschn93+github@gmail.com>
2019-11-14 12:23:30 +0100

    Adds associations & validations to models

---
+++ app/models/dose.rb
@@ -0,0 +1,9 @@
+class Dose < ApplicationRecord
+  belongs_to :ingredient
+  belongs_to :cocktail
+
+  validates :description, presence: true, allow_blank: false
+  validates :cocktail_id, :ingredient_id, presence: true
+  validates :cocktail_id, :uniqueness => { :scope => :ingredient_id }
+
+end
