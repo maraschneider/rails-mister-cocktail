3d89763170947fb2257ed57d27e83617edcd5357
Maraike Schneider <maschn93+github@gmail.com>
2019-11-17 12:26:04 +0100

    moves new-dose-form to cocktail page

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,8 +1,8 @@
 class DosesController < ApplicationController
-  def new
-    @cocktail = Cocktail.find(params[:cocktail_id])
-    @dose = Dose.new()
-  end
+  # def new
+  #   @cocktail = Cocktail.find(params[:cocktail_id])
+  #   @dose = Dose.new()
+  # end
 
   def create
     @cocktail = Cocktail.find(params[:cocktail_id])
@@ -11,7 +11,7 @@ class DosesController < ApplicationController
     if @dose.save
       redirect_to cocktail_path(@cocktail)
     else
-      render :new
+      render 'cocktails/show'
     end
   end
 
